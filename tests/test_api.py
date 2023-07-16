58e5530403d1a8844368954000bd744fc3f7040f
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2023-07-16 14:55:16 +0200

    Added tests for /generate rout

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,5 @@
+import re
+
 from unittest import TestCase
 from requests import Response
 from fastapi.testclient import TestClient
@@ -22,3 +24,47 @@ class TestApi(TestCase):
 
         self._run_std_response_assertions(response, 200)
         self.assertEqual(response.json(), {'message': 'Hello Password Generator!'})
+
+    def test_generate(self) -> None:
+        test_pattern = r"^([a-zA-Z0-9]){10}"
+        valid_params = {
+                "length": 10,
+                "lowercase": True,
+                "uppercase": False,
+                "digits": True,
+                "special": False,
+                }
+        response = self.client.post("/generate", json=valid_params)
+        self._run_std_response_assertions(response, 200)
+        data = response.json()
+
+        suggestion = data["password_suggestion"]
+        self.assertTrue(re.match(test_pattern, suggestion))
+        
+        invalid_params = {
+                "length": 0,
+                "lowercase": True,
+                "uppercase": False,
+                "digits": True,
+                "special": False,
+                }
+        response = self.client.post("/generate", json=invalid_params)
+        self._run_std_response_assertions(response, 500)
+        data = response.json()
+
+        detail = data["detail"]
+        self.assertEqual(detail, "Cannot generate password with 0 or negative length!")
+        
+        invalid_params = {
+                "length": 10,
+                "lowercase": False,
+                "uppercase": False,
+                "digits": False,
+                "special": False,
+                }
+        response = self.client.post("/generate", json=invalid_params)
+        self._run_std_response_assertions(response, 500)
+        data = response.json()
+
+        detail = data["detail"]
+        self.assertEqual(detail, "Cannot generate password from empty set of possible characters!")
