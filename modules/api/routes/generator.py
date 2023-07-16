f6b629ac0b930358ca74242ba9858e16cc30f5a4
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2023-07-16 14:56:03 +0200

    Developed /generate route

--- modules/api/routes/generator.py
+++ modules/api/routes/generator.py
@@ -1,3 +1,31 @@
+from fastapi import status
 from fastapi import APIRouter
+from fastapi import HTTPException
+from fastapi import Body
+from pydantic import BaseModel
+
+from modules.generator import generate
 
 router = APIRouter()
+
+class ResponseModel(BaseModel):
+    password_suggestion: str
+
+class GeneratorParams(BaseModel):
+    length: int = 12
+    lowercase: bool = True
+    uppercase: bool = True
+    digits: bool = True
+    special: bool = True
+
+@router.post("/generate", response_model=ResponseModel)
+def generate_password_sugestion(params: GeneratorParams):
+    suggestion = ""
+    try:
+        suggestion = generate(**params.dict())
+    except BaseException as e:
+        raise HTTPException(
+                status_code=500,
+                detail=str(e)
+            )
+    return ResponseModel(password_suggestion=suggestion) 
